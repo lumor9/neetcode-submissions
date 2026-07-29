92116e41444ac443c72f237058b3b850782b175e
morpf <146622916+lumor9@users.noreply.github.com>
2026-07-29 12:50:45 +0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map();
+
+        for (const num of nums){
+            map.set(num, (map.get(num) || 0) + 1)
+        }
+
+        const arr = Array.from({length: nums.length}, () => []);
+
+        const entries = map.entries();
+        for (const entry of entries){
+            arr[entry[1]-1].push(entry[0]);
+        }
+
+        const res = []
+        for (let i=arr.length-1; i>=0; i--){
+            for (let num of arr[i]){
+                res.push(num);
+                if (res.length === k) return res;
+            }
+        }
+
+        return res;
+
+    }
+}
